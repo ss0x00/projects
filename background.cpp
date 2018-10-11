576c74f2847e63eb46240638cb7e72f42b135f54
ss0x00 <37372738+ss0x00@users.noreply.github.com>
2018-10-12 01:44:51 +0900

    (c언어)(윈도우) 콘솔 배경화면 색 연속 바

---
+++ background.cpp
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#include <windows.h>
+
+void bg();
+
+int main(void)
+{
+	int i;
+	for(i=0; i<10; i++)
+	{
+		bg();
+	}
+	
+	return 0;
+}
+
+void bg()
+{
+	while(1)
+	{
+		SetConsoleTitle("ss0x00");
+		system("mode con cols=50 lines=20");
+		system("color 0f");
+		Sleep(50);
+		system("color 1f");
+		Sleep(50);
+		system("color 2f");
+		Sleep(50);
+		system("color 3f");
+		Sleep(50);
+		system("color 4f");
+		Sleep(50);
+		system("color 5f");
+		Sleep(50);
+		system("color 6f");
+		Sleep(50);
+		system("color 7f");
+		Sleep(50);
+		system("color 8f");	
+		Sleep(50);
+		system("color 9f");
+		Sleep(50);
+		system("color af");
+		Sleep(50);
+		system("color bf");
+		Sleep(50);
+		system("color cf");
+		Sleep(50);
+		system("color df");
+		Sleep(50);
+		system("color ef");
+		Sleep(50);
+		system("color ff");
+	}
+}
